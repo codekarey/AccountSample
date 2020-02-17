5083553b76ce14016bd74cee0dfa3aa9a78a613f
codekarey <codekarey@gmail.com>
2020-02-17 17:18:02 -0500

    Drafting relationships

---
+++ AccountSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccountSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick an account to explore.");
+
+            //Staff: bookstore, orders, register, requirements
+            //with infor on: studdent account / materials for each class or professor
+
+            //Professors: grades, date, time, review, location, class, credit, student limit
+            //with info on: bookstore, student account IN CLASS only
+
+            //Students: personal info, gpa, status, major, prerecs, tuition/scholarship
+            //with info on: searches for all classes & material & reviews
+        }
+    }
+}
